eaf869ffb2eabb9c0060f97ee9eff1e6d5906d67
Dmitriy <dima.komlyk@gmail.com>
2023-07-30 12:37:52 +0300

    Update routes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,16 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
-
-import { AppRoute } from '../../common/enums/enums';
-
 import { Tasks } from '../tasks/tasks';
-import { TaskPreView } from '../task-preview/task-preview';
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path={AppRoute.ROOT} element={<Tasks />}/>
-          <Route path={AppRoute.TASK_$ID} element={<TaskPreView />} />
+        <Route path='/' element={<Tasks />}/>
       </Routes>
     </>
   )
