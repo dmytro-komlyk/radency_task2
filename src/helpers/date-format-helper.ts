e8979fdd08e309f9129367b3772320438be257c0
Dmitriy <dima.komlyk@gmail.com>
2023-07-30 09:53:57 +0300

    Created date fortmat helper

---
+++ src/helpers/date-format-helper.ts
@@ -0,0 +1,17 @@
+export const getDateFormat = (date: string, format: string) => {
+    const newDate = new Date(date);
+
+    const map: { [key: string]: string } = {
+      mm: (newDate.getMonth() + 1).toString().padStart(2, '0'),
+      dd: newDate.getDate().toString().padStart(2, '0'),
+      yy: newDate.getFullYear().toString().slice(-2),
+      yyyy: newDate.getFullYear().toString(),
+  };
+    
+    return format.replace(/mm|dd|yyyy|yy/gi, (matched) => map[matched])
+  }
+  
+  export const getLocaleDateFormat = (date: string, format: any ) => {
+    const newDate = new Date(date);
+    return newDate.toLocaleDateString('en-US', format);
+  }
