e71ed8f19d82ce177e9317013adf4dab1f3c90aa
Dmitriy <dima.komlyk@gmail.com>
2023-07-31 15:30:16 +0300

    Added task form validate

--- src/components/tasks/components/task-preview/task-preview.tsx
+++ src/components/tasks/components/task-preview/task-preview.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Modal } from '../../../common/modal/modal';
 import { useSelector } from '../../../../store/store';
 import { selectTasksState } from '../../../../store/taskSlice';
+import { getValidateForm } from '../../../../helpers/validate-helper';
 
 interface ITaskPreviewProps {
   taskId: string | null,
@@ -20,25 +21,32 @@ const TaskPreview = ({ taskId = null, onTasktAdd, onTaskEdit, onTaskPreviewToggl
   }
 
   const [taskValues, setTaskValues] = useState(initTaskState);
+  const [errorsValues, setErrorsValues] = useState({ name: null, content: null, category: null });
   const [isSubmit, setIsSubmit] = useState(false);
 
   const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
-      const { name, value } = e.target as HTMLInputElement;
-      setTaskValues({
-        ...taskValues,
-        [name]: value,
-      });
+    const { name, value } = e.target as HTMLInputElement;
+
+    setTaskValues({
+      ...taskValues,
+      [name]: value,
+    });
+    setErrorsValues((state) => ({ ...state, [name]: null }))
   };
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
     setIsSubmit(true);
-    if (Boolean(taskId)) {
-      onTaskEdit(taskId as string, taskValues);
-    } else {
-      onTasktAdd(taskValues)
+    const { isErrors, errors } = getValidateForm(taskValues);
+    if (!isErrors) {
+      if (Boolean(taskId)) {
+        onTaskEdit(taskId as string, taskValues);
+      } else {
+        onTasktAdd(taskValues)
+      }
+      onTaskPreviewToggle(false);
     }
-    onTaskPreviewToggle(false);
+    setErrorsValues((state) => ({ ...state, ...errors }));
     setIsSubmit(false);
   }
 
@@ -50,30 +58,32 @@ const TaskPreview = ({ taskId = null, onTasktAdd, onTaskEdit, onTaskPreviewToggl
             type='text'
             name='name'
             id='name'
-            className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-slate-900 peer'
+            className={`${Boolean(errorsValues.name) ? 'border-red-300 focus:border-red-500' : 'border-gray-300 focus:border-slate-900'} block py-2.5 px-0 w-full text-sm text-gray-700 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 peer`}
             placeholder=' '
             value={taskValues.name}
             onChange={handleChange}
             required
           />
-          <label htmlFor='name' className='peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-slate-900 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>Name</label>
+          <label htmlFor='name' className={`${Boolean(errorsValues.name) ? 'text-red-500 peer-focus:text-red-500' : 'text-gray-500 peer-focus:text-slate-900'} peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6`}>Name</label>
+          {Boolean(errorsValues.name) && <span className="text-xs text-red-500">{errorsValues.name}</span>}
         </div>
         <div className='relative z-0 w-full mb-6 group'>
           <input
             type='text'
             name='content'
             id='content'
-            className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-slate-900 peer'
+            className={`${Boolean(errorsValues.content) ? 'border-red-300 focus:border-red-500' : 'border-gray-300 focus:border-slate-900'} block py-2.5 px-0 w-full text-sm text-gray-700 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 peer`}
             placeholder=' '
             value={taskValues.content}
             onChange={handleChange}
             required
           />
-          <label htmlFor='content' className='peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-slate-900 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>Content</label>
+          <label htmlFor='content' className={`${Boolean(errorsValues.content) ? 'text-red-500 peer-focus:text-red-600' : 'text-gray-500 peer-focus:text-slate-900'} peer-focus:font-medium absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6`}>Content</label>
+          {Boolean(errorsValues.content) && <span className="text-xs text-red-500">{ errorsValues.content }</span>}
         </div>
         <div className='relative z-0 w-full mb-6 group'>
-          <fieldset className='flex flex-col p-4 rounded border-2 border-slate-200'>
-            <legend className='p-1 my-0 mx-auto rounded bg-slate-300 text-white'>Select a task category</legend>
+          <fieldset className={`${Boolean(errorsValues.category) ? 'border-red-500' : 'border-slate-200'} flex flex-col p-4 rounded border-2`}>
+            <legend className={`${Boolean(errorsValues.category) ? 'bg-red-500' : 'bg-slate-200'} p-1 my-0 mx-auto rounded text-white`}>Select a task category</legend>
             <div className='flex gap-2'>
               <input
                 type='radio'
